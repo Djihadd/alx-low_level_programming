6a36b6dfd50ef6734a331e59e5b010568c2d004d
Djihadd <djihaddjio2@gmail.com>
2023-09-07 17:52:00 -0700

    all files done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry pointn 
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
