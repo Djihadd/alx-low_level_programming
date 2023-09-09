936508baf30813569fb4500ddeb93b7b1524e53e
Djihadd <djihaddjio2@gmail.com>
2023-09-09 01:28:47 -0700

    h

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio>
+
+/**
+ * main -Entry point
+ *
+ * Description: prints the lowercase alphabet in reverse
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
