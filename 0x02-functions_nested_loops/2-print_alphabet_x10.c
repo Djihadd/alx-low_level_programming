db891fd2592cc63c257d72cf8c94255977e59a75
Djihadd <djihaddjio2@gmail.com>
2023-09-12 09:33:01 -0700

    f3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
