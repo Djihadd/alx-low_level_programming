03cc1a5009982dc93122dafdef55496dbdb7b0ee
Djihadd <djihaddjio2@gmail.com>
2023-09-13 11:37:34 -0700

    done

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include"main"
+
+/**
+ * main - Entry point
+ *
+ * Descriptin: Write a program that prints the first 50
+ *              Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, su;
+
+	for (count = 0; count < 50; count++)
+	{
+		su = fib1 + fib2;
+		printf("%lu", su);
+
+		fib1 = fib2;
+		fib2 = su;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
