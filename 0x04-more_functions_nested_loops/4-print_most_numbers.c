67facb951dfd2c54b426a8d9b7ba4578cf302abc
Djihadd <djihaddjio2@gmail.com>
2023-09-14 10:37:47 -0700

    f4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,8 +2,8 @@
 
 /**
  * print_most_numbers - a function that prints the numbers, from 0 to 9
- *
- * Return: return result
+ *                                                   withou 2 and 4
+ * Return: 0 (success)
 */
 
 void print_most_numbers(void)
