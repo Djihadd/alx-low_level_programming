357f1bda46245fce7b884e552d8a33a4dca641e6
Djihadd <djihaddjio2@gmail.com>
2023-09-19 09:52:52 -0700

    f2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * void swap_int(int *a, int *b)
+ *
+ * @a: input parameter 1
+ * @b: input parameter 2
+ *
+ * Return: Nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temps;
+
+	temps = *a;
+	*a = *b;
+	*b = temp;
+}
