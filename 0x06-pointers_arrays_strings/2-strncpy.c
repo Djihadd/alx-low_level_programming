f91bab556305621f157af195854d850892aa5159
Djihadd <djihaddjio2@gmail.com>
2023-09-20 07:59:55 -0700

    f3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,9 +2,10 @@
 
 /**
  * _strncpy - function that copies a string.
- * 
+ *
  * @dest: 1st input
  * @src: 2nd input
+ * @n: 3ed input
  *
  * Return: result
 */
@@ -18,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (i < n)
 	{
-		dest[i] = '\0':
+		dest[i] = '\0';
 			i++;
 	}
 
