5d03e11357b67bea72907688d8f206450061ea5d
Djihadd <djihaddjio2@gmail.com>
2023-09-21 03:43:15 -0700

    f8

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,9 +3,29 @@
 /**
  * *leet - function that encodes a string into 1337.
  *
- * @c: input
+ * @c: inpt
  *
  * Return: result
 */
 
-char *leet(char *)
+char *leet(char *c)
+{
+	char  *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 1};
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			if (*c == key[i] || *c == key[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
+
+		}
+		c++;
+	}
+	return (cp);
+}
