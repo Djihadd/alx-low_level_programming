09de5da2a874a822d11ff22fb968d73239a49e66
Djihadd <djihaddjio2@gmail.com>
2023-09-25 12:59:22 -0700

    f8

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * _strstr - function that locates a substring.
+ *
+ * @a: input pointer
+ *
+ * Return: pointer to the beginning of the located substring
+ *          or NULL if the substring is not found.
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int itr, jtr;
+
+	for (itr = 0; itr < 8; itr++)
+	{
+		for (jtr = 0; jtr < 8; jtr++)
+		{
+			_putchar(a[itr][jtr]);
+		}
+		_putchar('\n');
+	}
+
+}
