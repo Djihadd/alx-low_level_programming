3f92d5e8889a99760ea5ada4bcd41f00209b1144
Djihadd <djihaddjio2@gmail.com>
2023-10-04 11:46:31 -0700

    f1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints programme name, followed a new line.
+ * @argc: int
+ * @argv: list
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
